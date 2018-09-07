792b7c693f35c963079fd2e10af65090135dc8f3
douglas321 <32646869+douglas321@users.noreply.github.com>
2018-09-07 09:10:11 -0600

    Add files via upload

---
+++ bash/addition.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+clear
+echo "hello, welcome to my BASH addition experiment program"
+x=$1
+y=$2
+echo The sum is $(( $x + $y ))
--- bash/updater.sh
+++ bash/updater.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 clear
-echo Hello, welcome to my BASH updater script. Would you like to update
+echo Hello, welcome to my BASH updater script. Would you like to update?[y/n]
 read update
-sudo apt update
-sudo apt upgrade -y
-sudo apt autoclean
-sudo apt autoremove
+if [[ "$update" == "y" ]]
+then sudo apt update
+else echo "Alrighty"
+fi
+
+echo Upgrade?[y/n]
+read upgrade
+if [[ "$upgrade" == "y" ]]
+then sudo apt upgrade
+else echo "Alrighty"
+fi
+
+echo autoclean?[y/n]
+read autoclean
+if [[ "$autoclean" == "y" ]]
+then sudo apt autoclean
+else echo "Alrighty"
+fi
+
+
+echo autoremove?[y/n]
+read autoremove
+if [[ "$autoremove" == "y" ]]
+then sudo apt autoremove
+else echo "Alrighty"
+fi
